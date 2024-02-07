291a83b28b758cf97d83125bb55cf6722d7e0bd4
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-02-07 15:43:31 +0300

    Tables redesigned

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Login from './components/Login';
+// import Login from './components/Login';
 import PatientForm from './components/PatientForm';
 import PatientTable from './components/PatientTable';
 import Login from './components/Login';
@@ -7,20 +7,23 @@ import DoctorForm from './components/DoctorForm';
 import DoctorTable from './components/DoctorTable';
 import AppointmentForm from './components/AppointmentForm'
 import AppointmentTable from './components/AppointmentTable';
-import SignUp from './components/SignUp';
+// import SignUp from './components/SignUp';
 import { Route, Switch } from "react-router";
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      <Switch>
+      {/* <Switch>
         <Route exact path = "/patients">
           <PatientForm/>
           <PatientTable />
         </Route>
-        <Route exact path = "/patients/:id">
+        <Route exact path = "/patients/:id"> */}
+          <PatientForm/>
           <PatientTable/>
-        </Route>
+          <DoctorTable/>
+        {/* </Route>
         <Route path="/staffs">
           <DoctorForm/>
           <DoctorTable/>
@@ -43,7 +46,7 @@ function App() {
         </Route>
         <Route exact path="/logout"/>
         <Route exact path="/checkSession"/>
-      </Switch>
+      </Switch> */}
       
       {/* <Login/> */}
     </div>
